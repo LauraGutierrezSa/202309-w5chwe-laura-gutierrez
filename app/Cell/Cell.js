65a86728336fa6b65fd6e1154d706ab6b00d7d04
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-10-16 01:58:21 +0200

    Add cell neighbours' methods into Cell class

--- app/Cell/Cell.js
+++ app/Cell/Cell.js
@@ -1,8 +1,30 @@
 class Cell {
   isAlive;
+  aliveNeighbours = 0;
+  neighbourCoordinates;
 
   constructor(isAlive) {
     this.isAlive = isAlive;
+  }
+
+  setStatus(status) {
+    this.isAlive = status;
+  }
+
+  getStatus() {
+    return this.isAlive;
+  }
+
+  getAliveNeighbours() {
+    return this.aliveNeighbours;
+  }
+
+  increaseAliveNeighbours() {
+    this.aliveNeighbours++;
+  }
+
+  resetAliveNeighbours() {
+    this.aliveNeighbours = 0;
   }
 }
 
--- app/Cell/Cell.test.js
+++ app/Cell/Cell.test.js
@@ -22,15 +22,56 @@ describe("Given the Cell class", () => {
     cell.isAlive = false;
     expect(cell.isAlive).toBe(false);
   });
-});
 
-describe("When changing the isAlive property of the cell to false", () => {
-  test("Then cell.isAlive returns false", () => {
-    const expectedIsAlive = false;
-    const cell = new Cell(true);
+  describe("Given a Cell class", () => {
+    let cell;
+    describe("When setting the status of the cell", () => {
+      test("Then it should update the status of the cell", () => {
+        cell = new Cell(false);
 
-    cell.isAlive = false;
+        cell.setStatus(true);
 
-    expect(cell.isAlive).toBe(expectedIsAlive);
+        expect(cell.getStatus()).toBe(true);
+      });
+    });
+  });
+
+  describe("When changing the isAlive property of the cell to false", () => {
+    test("Then cell.isAlive returns false", () => {
+      const expectedIsAlive = false;
+      const cell = new Cell(true);
+
+      cell.isAlive = false;
+
+      expect(cell.isAlive).toBe(expectedIsAlive);
+    });
+  });
+
+  describe("Given a Cell class", () => {
+    let cell;
+
+    describe("When increasing the count of alive neighbors", () => {
+      test("Then it should increment the count of alive neighbors by one", () => {
+        cell = new Cell(false);
+
+        cell.increaseAliveNeighbours();
+
+        expect(cell.getAliveNeighbours()).toBe(1);
+      });
+    });
+  });
+
+  describe("Given a Cell class", () => {
+    let cell;
+    describe("When resetting the count of alive neighbors", () => {
+      test("Then it should reset the count of alive neighbors to zero", () => {
+        cell = new Cell(false);
+
+        cell.increaseAliveNeighbours();
+        cell.resetAliveNeighbours();
+
+        expect(cell.getAliveNeighbours()).toBe(0);
+      });
+    });
   });
 });
